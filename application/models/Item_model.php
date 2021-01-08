cff3ce29f2e5b039e1a87071e69bea5b9b970bec
yaserantariksa <53243069+yaserant@users.noreply.github.com>
2021-01-08 22:46:16 +0700

    item img not define

--- application/models/Item_model.php
+++ application/models/Item_model.php
@@ -114,7 +114,7 @@ class item_model extends CI_Model
             'item_harbel' => $post['item_harbel'],
             'item_harjual1' => $post['item_harjual1'],
             'item_harjual2' => $post['item_harjual2'],
-            'item_img' => $post['item_img'],
+            // 'item_img' => $post['item_img'],
             'updated' => date('Y-m-d H:i:s')
         ];
         if($post['item_img' != null ]) {
